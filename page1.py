b99656b489e4512f814ba9949b90798a7724c295
juarezbarros <sba24105@student.cct.ie>
2025-05-24 13:16:06 +0000

    Update page1.py

--- page1.py
+++ page1.py
@@ -2,25 +2,20 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import streamlit as st
 
-# Carregar o dataframe
+
 df = pd.read_csv("df_merged.csv")
 df['Date'] = pd.to_datetime(df['Date'])  # Converter para datetime
-
-# Filtrar datas a partir de 1º de dezembro de 2020
 df_filtered = df[df['Date'] >= '2020-01-01']
 
-# Título do app
-st.title("Fechamento por Ticker (Dezembro 2020 em diante)")
-
-# Criar o gráfico com Matplotlib
+st.title("Close Price - Stocks 2020")
 plt.figure(figsize=(14, 7))
 
 for ticker in df_filtered['Ticker'].unique():
     ticker_df = df_filtered[df_filtered['Ticker'] == ticker]
     plt.plot(ticker_df['Date'], ticker_df['Close'], label=ticker)
 
-plt.xlabel('Data')
-plt.ylabel('Preço de Fechamento')
+plt.xlabel('Date')
+plt.ylabel('Close Price')
 plt.title('Preço por Ticker')
 plt.legend()
 plt.grid(True)
